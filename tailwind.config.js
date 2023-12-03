65d66657c70a2bd88deeb3df957cd48420105261
Styaaa <s160421048@student.ubaya.ac.id>
2023-12-03 22:00:04 +0800

    Background Fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {},
     backgroundImage: {
-      'background': "url('Coolture/img/bg.jpg')",
+      'background': "url('../img/bg.jpg')",
     }
   },
   plugins: [],
